3697bbce47b022d45e617ef20570c4935e740897
soudegesu <soudegesu@hotmail.co.jp>
2022-01-22 22:53:34 +0900

    use popover

--- macos-tooltip-practice/ContentView.swift
+++ macos-tooltip-practice/ContentView.swift
@@ -8,16 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isShowPopover = false
+  
     var body: some View {
-      VStack(spacing: 30) {
-        Button("button1", action: {
-          
-        })
-        .help("This is tooltip on macOS")
-        Button("button2", action: {
-          
-        }).toolTip("This is custom tooltip")
-      }.frame(width: 480, height: 360, alignment: .center)
+        VStack(spacing: 30) {
+            Button("button1", action: {
+              
+            })
+            .help("This is tooltip on macOS")
+
+            Button("button2", action: {
+              
+            })
+            .onHover(perform: { hovering in
+              isShowPopover = hovering
+            })
+            .popover(isPresented: $isShowPopover, attachmentAnchor: .point(.bottom), arrowEdge: .bottom) {
+              Text("This is popover")
+                .padding(10)
+            }
+        }
+        // Need at least 1 space
+        .padding(.bottom, 1)
+        .frame(width: 480, height: 360, alignment: .center)
     }
 }
 
